aca920c4a1500740e7e2ae21504ba6675a7acc93
YngwieJM <yngwiejmantiri@gmail.com>
2026-03-05 01:34:30 +0800

    authentication with Token

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const ordersController = require("../controllers/orders.controller");
+const { authenticate, authorize } = require("../middleware/auth.middleware");
 
-router.post("/", ordersController.createOrder);
+router.post("/",authenticate, authorize("WAITER", "MANAGER"), ordersController.createOrder);
 
-router.post("/:id/items", ordersController.addItemOrder);
+router.post("/:id/items",authenticate, authorize("WAITER"), ordersController.addItemOrder);
 
-router.get("/", ordersController.getAllOrders);
+router.get("/",authenticate, authorize("MANAGER"), ordersController.getAllOrders);
 
-router.get("/:id", ordersController.getOrderById);
+router.get("/:id",authenticate,authorize("WAITER", "MANAGER"), ordersController.getOrderById);
 
-router.patch("/:id/close", ordersController.closeOrder);
+router.patch("/:id/close",authenticate ,authorize("WAITER", "MANAGER"), ordersController.closeOrder);
 
 module.exports = router;
--- routes/staff.routes.js
+++ routes/staff.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const staffController = require("../controllers/staff.controller");
+const  {authenticate, authorize } = require("../middleware/auth.middleware");
 
 router.post("/", staffController.createStaff);
 router.get("/", staffController.getAllStaff);
 router.get("/:id", staffController.getStaffById);
 router.put("/:id", staffController.updateStaff);
-router.delete("/:id", staffController.deleteStaff);
+router.delete("/:id",authenticate, authorize("MANAGER"), staffController.deleteStaff);
 
 module.exports = router;
