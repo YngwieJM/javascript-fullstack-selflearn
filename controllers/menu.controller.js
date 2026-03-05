743200a101883b6728d6a9c02653456566c09672
YngwieJM <yngwiejmantiri@gmail.com>
2026-03-05 21:27:46 +0800

    menu service

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -1,21 +1,18 @@
 const pool = require("../config/db");
+const menuService = require("../services/menu.service");
 
 exports.createMenuItem = async(req, res) => {
     const {name, category, price} = req.body;
 
-    if(!name || !category || !price){
-        return res.status(400).json({message: "All field required"});
-    }
-
     try{
-        const result = await pool.query(
-            `INSERT INTO menu_items (name, category, price)
-            VALUES($1, $2, $3)
-            RETURNING *`, [name, category, price]
-        );
+        const item = await menuService.createMenuItem(name, category, price);
 
-        res.status(201).json(result.rows[0]);
+        res.status(201).json(item);
     }catch(err){
+
+        if(err.message === "INVALID_MENU_DATA"){
+            return res.status(400).json({message: "Invalid Menu Item"});
+        }
         console.error(err);
         res.status(500).json({message: "Internal Server Error"});
     }
@@ -23,13 +20,8 @@ exports.createMenuItem = async(req, res) => {
 
 exports.getAllMenuItems = async (req, res) => {
     try{
-        const result = await pool.query(
-            `SELECT * FROM menu_items
-            WHERE is_available = TRUE
-            ORDER BY id ASC`
-        );
-
-        res.json(result.rows);
+        const item = await menuService.getAllMenuItems();
+        res.json(item);
     }catch(err){
         console.error(err);
         res.status(500).jsone({message: "Internal Server Error"});
@@ -37,64 +29,69 @@ exports.getAllMenuItems = async (req, res) => {
 };
 
 exports.getMenuItemById = async (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
 
     try{
-        const result = await pool.query(
-            `SELECT * FROM menu_items WHERE id = $1`, [id]
-        );
-
-        if(result.rows.length === 0){
-            return res.status(404).json({message: "Menu item not found"});
-        }
-
-        res.json(result.rows[0]);
+        const item = await menuService.getMenuItemById();
+        res.json(item);
     }catch(err){
+
+        if(err.message === "MENU_ITEM_NOT_FOUND"){
+            return res.json(404).json({message: "Menu Item not found"});
+        }
         console.error(err);
         res.status(500).json({message: "Internal Server Error"});
     }
 };
 
 exports.updateMenuItem = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const {name, category, price, is_available} = req.body;
+    const id = req.params.id;
+    const {name, category, price} = req.body;
 
     try{
-        const result = await pool.query(
-            `UPDATE menu_items
-            SET name = COALESCE($1, name),
-                category = COALESCE($2, category),
-                price = COALESCE($3, price),
-                is_available = COALESCE($4, is_available)
-            WHERE id = $5 RETURNING *`, [name, category, price, is_available, id]
-        );
+        const item = await menuService.updateMenuItem(id, name, category, price);
 
-        if(result.rows.length === 0){
+        res.json(item);
+    }catch(err){
+
+        if(err.message ==="MENU_ITEM_NOT_FOUND"){
             return res.status(404).json({message: "Menu item not found"});
         }
-
-        res.json(result.rows[0]);
-    }catch(err){
         console.error(err);
         res.status({message: "Internal Server Error"});
     }
 };
 
-exports.deleteMenuItem = async (req, res) => {
-    const id = parseInt(req.params.id);
+exports.toggleAvailablity = async (req, res) => {
+    const id = req.params.id;
+    const {is_available} = req.body;
 
     try{
-        const result = await pool.query(
-            `UPDATE menu_items
-            SET is_available = FALSE
-            WHERE id = $1 RETURNING *`, [id]
-        );
-        if(result.rows.length === 0){
-            return res.status(404).json({message: "Menu item not found"});
+        const item = await menuService.toggleAvailability(id, is_available);
+        res.json(item);
+    }catch(err){
+        if(err.message === "MENU_ITEM_NOT_FOUND"){
+            return res.status(404).json({message:"Menu item not found"})
         }
 
-        res.json({message: "Menu item delted successfully", item:result.rows[0]})
+        console.error(err);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+exports.deleteMenuItem = async (req, res) => {
+    const id = req.params.id
+
+    try{
+        const item = await menuService.deleteMenuItem(id);
+        res.json({message: "Item deleted successfully", item});
+
     }catch(err){
+
+        if(err.message === "MENU_ITEM_NOT_FOUND"){
+            return res.status(404).json({message:"Menu item not found"})
+            }
+
         console.error(err);
         res.status(500).json({message: "Internal Server Error"});
     }
--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const menuController = require("../controllers/menu.controller");
+const {authenticate, authorize} = require("../middleware/auth.middleware");
 
-router.post("/", menuController.createMenuItem);
-router.get("/", menuController.getAllMenuItems);
-router.get("/:id", menuController.getMenuItemById);
-router.patch("/:id", menuController.updateMenuItem);
-router.delete("/:id", menuController.deleteMenuItem);
+router.post("/", authenticate, authorize("MANAGER"), menuController.createMenuItem);
+router.get("/", authenticate, authorize("WAITER", "BARTENDER", "MANAGER"), menuController.getAllMenuItems);
+router.get("/:id", authenticate, authorize("WAITER", "BARTENDER", "MANAGER"), menuController.getMenuItemById);
+router.patch("/:id", authenticate, authorize("MANAGER"),  menuController.updateMenuItem);
+router.patch("/:id/availability", authenticate, authorize("MANAGER"), menuController.toggleAvailablity);
+router.delete("/:id", authenticate, authorize("MANAGER"), menuController.deleteMenuItem);
+
+// no security for these routes
+// router.post("/", menuController.createMenuItem);
+// router.get("/", menuController.getAllMenuItems);
+// router.get("/:id", menuController.getMenuItemById);
+// router.patch("/:id",  menuController.updateMenuItem);
+// router.patch("/:id/availability", menuController.toggleAvailablity);
+// router.delete("/:id", menuController.deleteMenuItem);
 
 module.exports = router;
