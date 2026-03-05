1e942386bf89febeba1ee6d29d6795fb38d4d97b
YngwieJM <yngwiejmantiri@gmail.com>
2026-03-05 23:02:37 +0800

    staff service

--- routes/staff.routes.js
+++ routes/staff.routes.js
@@ -4,10 +4,10 @@ const staffController = require("../controllers/staff.controller");
 const  {authenticate, authorize } = require("../middleware/auth.middleware");
 
 router.get("/", authenticate, authorize("MANAGER"), staffController.getAllStaff);
-router.get("/:id", authenticate. authorize("MANAGER"), staffController.getStaffById);
+router.get("/:id", authenticate, authorize("MANAGER"), staffController.getStaffById);
 router.post("/", authenticate, authorize("MANAGER"), staffController.createStaff);
-router.put(":/id", authenticate, authorize("MANAGER"), staffController.updateStaff);
-router.patch(":/id", authenticate, authorize("MANAGER"), "WAITER", "BARTENDER"), staffController.updatePassword);
+router.put("/:id", authenticate, authorize("MANAGER"), staffController.updateStaff);
+router.patch("/:id", authenticate, authorize("MANAGER", "WAITER", "BARTENDER"), staffController.updatePassword);
 router.delete("/:id", authenticate, authorize("MANAGER"), staffController.deleteStaff);
 
 // router.post("/", staffController.createStaff);
